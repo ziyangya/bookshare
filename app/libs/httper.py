799e3736e3733bfb7cfcc287a486ebcedc9e37f1
zyeoh <zyeoh191@163.com>
2019-10-23 21:01:48 +0800

    使用第三方requests模块发送HTTP请求

--- app/libs/httper.py
+++ app/libs/httper.py
@@ -9,8 +9,13 @@ import requests
 
 
 class HTTP:
+    """
+    使用requests发送HTTP请求
+    """
     @staticmethod
     def get(url, return_json=True):
+        # return_json控制返回结果是否需要转换为json格式
+        # r是对HTTP请求结果的封装，包含很多信息
         r = requests.get(url)
         if r.status_code != 200:
             return {} if return_json else ''
